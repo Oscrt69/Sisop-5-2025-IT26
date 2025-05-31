a715a939c3c9a40be30ad0813fc7c8c383f6ccab
livai12 <naufalardhana11@gmail.com>
2025-05-31 20:33:33 +0700

    Update kernel.c

--- src/kernel.c
+++ src/kernel.c
@@ -8,15 +8,50 @@ int main() {
 
 void printString(char *str)
 {
-  //TODO: Implementasi fungsi untuk mencetak string
+  int i = 0;
+  while (str[i] != '\0') {
+    // panggil interrupt 0x10, AH=0x0E (teletype), AL=karakter
+    _interrupt(0x10, 0x0E00 + str[i], 0, 0, 0);
+    i++;
+  }
 }
 
 void readString(char *buf)
 {
-  //TODO: Implementasi fungsi untuk membaca string
+  int i = 0;
+  char ch;
+
+  while (1) {
+    // panggil interrupt 0x16, AH=0x00 untuk membaca karakter
+    ch = _interrupt(0x16, 0x0000, 0, 0, 0) & 0xFF;
+
+    if (ch == '\r') { // enter
+      buf[i] = '\0';
+      _interrupt(0x10, 0x0E00 + '\r', 0, 0, 0); // tampilkan enter
+      _interrupt(0x10, 0x0E00 + '\n', 0, 0, 0);
+      break;
+    }
+    else if (ch == '\b') { // backspace
+      if (i > 0) {
+        i--;
+        _interrupt(0x10, 0x0E00 + '\b', 0, 0, 0);
+        _interrupt(0x10, 0x0E00 + ' ', 0, 0, 0);
+        _interrupt(0x10, 0x0E00 + '\b', 0, 0, 0);
+      }
+    }
+    else {
+      buf[i] = ch;
+      _interrupt(0x10, 0x0E00 + ch, 0, 0, 0); // tampilkan karakter
+      i++;
+    }
+  }
 }
 
 void clearScreen()
 {
-  //TODO: Implementasi fungsi untuk membersihkan layar
+  int i;
+  for (i = 0; i < 80 * 25; i++) {
+    _putInMemory(0xB000, i * 2, ' ');     // karakter kosong
+    _putInMemory(0xB000, i * 2 + 1, 0x07); // warna abu-abu terang
+  }
 }
