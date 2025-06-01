9e49d693efe5856a2a30abddc52bb1542a2689f7
Oscarrrrrrrrrrrrrrrr <oscaryavat3@gmail.com>
2025-06-01 09:33:39 +0700

    Update shell.c

--- src/shell.c
+++ src/shell.c
@@ -2,8 +2,14 @@
 #include "kernel.h"
 #include "std_lib.h"
 
+char current_user[32] = "user";
+char grand_company[32] = "";
+byte current_color = 0x07;
+
 void shell(){
   char buf[128];
+  char cmd[64];
+  char args[2][64];
   printString("Welcome to EorzeOS!\n");
   while (true) {
     printString("> ");
@@ -14,7 +20,22 @@ void shell(){
 }
 
 void parseCommand(char *buf, char *cmd, char arg[2][64]) {
-  //TODO: Implementasi fungsi untuk mem-parsing perintah
+  int i = 0, j = 0, arg_num = 0;
+    while (buf[i] == ' ') i++;
+    while (buf[i] != ' ' && buf[i] != '\0') {
+        cmd[j++] = buf[i++];
+    }
+    cmd[j] = '\0';
+    
+    while (buf[i] != '\0' && arg_num < 2) {
+        while (buf[i] == ' ') i++;
+        if (buf[i] == '\0') break;
+        
+        j = 0;
+        while (buf[i] != ' ' && buf[i] != '\0') {
+            arg[arg_num][j++] = buf[i++];
+        }
+        arg[arg_num][j] = '\0';
+        arg_num++;
+    }
 }
-
-// Tambahkan fungsi bila perlu
