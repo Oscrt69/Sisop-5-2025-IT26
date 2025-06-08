8a42bd3da319b14552fd4a61b9df2ae965a8cfe2
Oscarrrrrrrrrrrrrrrr <oscaryavat3@gmail.com>
2025-06-08 23:39:15 +0700

    Update std_lib.c

--- src/std_lib.c
+++ src/std_lib.c
@@ -1,34 +1,26 @@
-// std_lib.c
 #include "std_lib.h"
 
 int div(int a, int b) {
-    int result = 0;
-    int sign = 1;
+    int result, sign;
     if (b == 0) return 0;
-
+    sign = ((a < 0) ^ (b < 0)) ? -1 : 1;
+    result = 0;
     if (a < 0) { a = -a; sign *= -1; }
     if (b < 0) { b = -b; sign *= -1; }
-
     while (a >= b) {
         a -= b;
         result++;
     }
 
-    return result * sign;
+    return result * sign; 
 }
 
+
 int mod(int a, int b) {
-    int sign = (a < 0) ? -1 : 1;
+    int c;
     if (b == 0) return 0;
-
-    a = (a < 0) ? -a : a;
-    b = (b < 0) ? -b : b;
-
-    while (a >= b) {
-        a -= b;
-    }
-
-    return a * sign;
+    c = a - div(a, b) * b;
+    return c;
 }
 
 bool strcmp(char *str1, char *str2) {
@@ -42,37 +34,41 @@ bool strcmp(char *str1, char *str2) {
 
 void strcpy(char *dst, char *src) {
     while (*src) {
-        *dst = *src;
-        dst++;
-        src++;
+        *dst++ = *src++;
     }
     *dst = '\0';
 }
 
 void clear(byte *buf, unsigned int size) {
-    for (int i = 0; i < size; i++) {
+    unsigned int i;
+    for (i = 0; i < size; i++) {
         buf[i] = 0;
     }
 }
 
 void atoi(char *str, int *num) {
-    int i = 0, result = 0, sign = 1;
-
-    if (str[0] == '-') {
+    int i = 0; 
+    int result = 0; 
+    int sign = 1;
+    if (str[i] == '-') {
         sign = -1;
         i++;
+    } else if (str[i] == '+') {
+        i++;
     }
-
     while (str[i] >= '0' && str[i] <= '9') {
         result = result * 10 + (str[i] - '0');
         i++;
     }
-
+    while (str[i]== ' '){
+        i++;
+    }
     *num = result * sign;
 }
 
 void itoa(int num, char *str) {
-    int i = 0, isNegative = 0;
+    int i = 0, j, k;
+    int isNegative = 0;
     if (num == 0) {
         str[i++] = '0';
         str[i] = '\0';
@@ -81,7 +77,7 @@ void itoa(int num, char *str) {
 
     if (num < 0) {
         isNegative = 1;
-        num = -num;
+        num = 0 - num;
     }
 
     while (num != 0) {
@@ -93,10 +89,10 @@ void itoa(int num, char *str) {
 
     str[i] = '\0';
 
-    // Reverse string
-    for (int j = 0, k = i - 1; j < k; j++, k--) {
+    for (j = 0, k = i - 1; j < k; j++, k--) {
+        char *str;
         char temp = str[j];
         str[j] = str[k];
         str[k] = temp;
     }
-}
+}      
